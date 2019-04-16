9a6b492e694757d63e00a806139a76a2c922d0a8
Say-hi <782108446@qq.com>
2019-04-16 23:13:45 +0800

    1555427624682

--- src/js/income.js
+++ src/js/income.js
@@ -6,68 +6,47 @@ let baseDomain = 'http://teach.idwenshi.com/teach/api/web/index.php'
 let tabIndex = 1
 let page = 0
 let list = []
-let more = 0
+let nomore = 0
+let str = "<div class='tac f28 c999'>--- 哎呀，没有相关数据了 ---</div>"
 function getUrlParam(name) {
   let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
   let r = window.location.search.substr(1).match(reg);
   if (r != null) return decodeURI(r[2]); return null;
 }
-let data = [
-  {
-    number: '1234234',
-    money: 100.00,
-    status: 1,
-    time: new Date()
-  },
-  {
-    number: '1234234',
-    money: 100.00,
-    status: 2,
-    time: new Date()
-  },
-  {
-    number: '1234234',
-    money: 100.00,
-    status: 3,
-    time: new Date().toLocaleTimeString()
-  },
-  {
-    number: '1234234',
-    money: 100.00,
-    status: 1,
-    time: new Date().toLocaleDateString()
-  },
-  {
-    number: '1234234',
-    money: 100.00,
-    status: 1,
-    time: new Date().toLocaleString()
-  },
-  {
-    number: '1234234',
-    money: 100.00,
-    status: 3,
-    time: new Date().toLocaleString()
-  }
-]
 let starData = `${new Date().getFullYear()}/${new Date().getMonth() + 1}/${new Date().getDate()}`
 let endData = `${new Date().getFullYear()}/${new Date().getMonth() + 1}/${new Date().getDate()}`
 
-function getData(obj) {
+function getData(obj = {}) {
+  $('.modal').show();
   return new Promise((resolve, reject) => {
     $.post(`${baseDomain}/user/profit-detail`, {
       uid: localStorage.getItem('mini_id'),
       where: tabIndex,
+      page: ++page,
       out_trade_no: obj.out_trade_no || null,
-      time_start: obj.time_start || null,
-      time_end: obj.time_end || null
+      time_start: tabIndex == 2 ? new Date().getTime() / 1000 : obj.time_start || null,
+      time_end: tabIndex == 2 ? 0 : obj.time_end || null
     }, function (res) {
       for (let v of res.data.lists) {
         v.create_time = new Date(v.create_time * 1000).toLocaleString()
         v.finish_time = new Date(v.finish_time * 1000).toLocaleString()
       }
-      list = list.concat(res.data.lists)
-      more = res.data.lists.length <= res.data.pre_page ? 1 : 0
+      // list = list.concat(res.data.lists)
+      nomore = res.data.lists.length < res.data.pre_page ? 1 : 0
+      let item = template('item', {target: res.data.lists});
+      setTimeout(function () {
+        if (!res.data.lists.length) {
+          if (page == 1) {
+            $('.container').html(str)
+          } else {
+            $('.container').append(str)
+          }
+        } else {
+          (obj.type == 'refresh' || page == 1 )? $('.container').html(item) : $('.container').append(item);
+          if (nomore >= 1) {$('.container').append("<div class='tac f28 c999 p20'>--- 别扯了，到底了 ---</div>")}
+        }
+        $('.modal').hide();
+      }, 1000)
       resolve(res)
     })
   })
@@ -79,21 +58,27 @@ function scrollPage () {
 //      var windowHeight = $(this).height();
   if ($(this).scrollTop() + $(this).height() == $(document).height()) {  //滚动到底部执行事件
     console.dir("我到底部了")
-    $('.modal').show()
-    setTimeout(()=> {
-      let item = template('item', {target: data})
-      $('.container').append(item)
-      $('.modal').hide()
-    }, 2000)
+    if (nomore >= 1) {
+      alert('没有更多数据了')
+    } else {
+      getData()
+    }
+    // setTimeout(()=> {
+    //   let item = template('item', {target: data})
+    //   $('.container').append(item)
+    //   $('.modal').hide()
+    // }, 2000)
   }
   if ($(this).scrollTop() == 0) {  //滚动到头部部执行事件
     console.dir("我到头部了")
-    $('.modal').show()
-    setTimeout(()=> {
-      let item = template('item', {target: data})
-      $('.container').html(item)
-      $('.modal').hide()
-    }, 500)
+    page = 0
+    nomore = 0
+    getData({type: 'refresh'})
+    // setTimeout(()=> {
+    //   let item = template('item', {target: data})
+    //   $('.container').html(item)
+    //   $('.modal').hide()
+    // }, 500)
   }
 }
 
@@ -109,13 +94,7 @@ $(function () {
   $('#time11').val(starData)
   $('#time22').val(endData)
 
-  $('.modal').show();
-  getData({}).then(res => {
-    console.log(res)
-    let item = template('item', {target: list});
-    $('.container').html(item);
-    $('.modal').hide();
-  })
+  getData()
 
 
 
@@ -124,13 +103,9 @@ $(function () {
     $('.record-tab-tab span').removeClass('c2ca2f2');
     $(this).addClass('c2ca2f2');
     tabIndex = $(this).attr('data-type')
-
-    $('.modal').show();
-    let item = template('item', {target: data});
-    setTimeout(function () {
-      $('.container').html(item);
-      $('.modal').hide();
-    }, 1000)
+    page = 0
+    nomore = 0
+    getData({type: 'refresh'})
   })
 
 
@@ -144,25 +119,31 @@ $(function () {
   })
 
   $('.confirm').click(function () {
-
     $('.send-email-mask').hide()
   })
 
   $(window).scroll(scrollPage)
+
   $('.item').click(function () {
     console.log($(this).html())
   })
+
   $('.choose-btn').click(function () {
     $('.icon-xiangyou').toggleClass('show')
     $('.choose-time').toggleClass('height')
   })
+
   $('.choose-time-confirm-btn').click(function () {
     if (new Date(starData).getTime() > new Date(endData).getTime()) {
       return alert('【截至时间】不能超过【起始时间】')
     }
     $('.icon-xiangyou').toggleClass('show')
     $('.choose-time').toggleClass('height')
+    page = 0
+    nomore = 0
+    getData({type: 'refresh', time_start: new Date(starData).getTime() / 1000, time_end: new Date(endData).getTime() / 1000})
   })
+
   new Mdate('time1', {
     acceptId: 'time11',
     format: '/',
