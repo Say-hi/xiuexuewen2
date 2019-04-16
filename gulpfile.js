683abdfd8554931eb628dc83d22b5d6a06060fea
Say-hi <782108446@qq.com>
2019-04-16 14:59:52 +0800

    1555397992341

--- gulpfile.js
+++ gulpfile.js
@@ -101,4 +101,4 @@ gulp.task('watch',['clean', 'rev-html'], function () {
   gulp.watch('src/**/*.less', ['rev-html'])
 })
 
-gulp.task('rev', gulpSequence('rev-html', 'build','add', 'commit', 'push'));
+gulp.task('rev', gulpSequence('clean', 'rev-html', 'build','add', 'commit', 'push'));
